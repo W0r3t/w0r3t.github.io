448791bd7feabdad4e52b9def881bbfb6f2f128e
W0r3t <47696509+W0r3t@users.noreply.github.com>
2024-08-17 19:41:37 +0200

    initial-comit

---
+++ assets/js/config.js
@@ -0,0 +1,35 @@
+const BADGES = [
+  {
+    name: "HypeSquad Bravery",
+    image: "assets/badges/hypesquad_bravery.svg",
+  },
+  {
+    name: "goober hunter", 
+    image: "assets/badges/bug_hunter_level2.svg", 
+  },
+  {
+    name: "king of silly",
+    image: "assets/badges/server_owner.svg",
+  },
+];
+
+const USER_ID = "421305540430397460"; 
+const BACKGROUND_IMAGE = "assets/images/dog.jpg"; 
+const STATUS = {
+  dnd: {
+    name: "Do not disturb",
+    color: "#ed4245",
+  },
+  idle: {
+    name: "Busy",
+    color: "#faa81a",
+  },
+  online: {
+    name: "Online",
+    color: "#3ba55d",
+  },
+  offline: {
+    name: "Offline",
+    color: "#6e7985",
+  },
+};
